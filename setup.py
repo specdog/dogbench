67d468f505441d1f79d8f286b9fbc0d38772b733
Justin Diclemente <logohere@gmail.com>
2026-06-18 20:08:50 -0400

    chore: bump to 0.2.0, fix MIT license, add npm metadata, guide tab, consistent rows

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name="dogbench",
-    version="0.1.0",
+    version="0.2.0",
     packages=find_packages(),
     py_modules=["telemetry"],
     scripts=["dogbench"],
